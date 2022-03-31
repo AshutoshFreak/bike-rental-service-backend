ee8c12eca2264d2750d7a033b2344b2da07c0d59
divyanshk443 <55971596+divyanshk443@users.noreply.github.com>
2022-03-31 17:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,19 +9,17 @@ app.use(express.json());
 app.post("/addbike",async(req,res)=>{
   try{
      const addbike=await pool.query(
-        'INSERT INTO "Users" ("UserId","EmailId", "MoneyDue")  VALUES ($1, $2,$3)', [req.body.UserId, req.body.Emailid,req.body.MoneyDue]); 
+        'INSERT INTO "Bikes" ("BikeId","BikeStatus", "LastStationDocked","LastStationDockedTimestamp","CreatedAt")  VALUES ($1, $2,$3,$4,$5)', [req.body.BikeId, req.body.BikeStatus,req.body.LastStationDocked,req.body.LastStationDockedTimestamp,req.body.CreatedAt]); 
         console.log(addbike);
   }catch(err)
   {
-      console.err(err.message);
+      console.error(err.message);
   }
 
-})
-
-//
-app.get("/view",async(req,res)=>{
+})//getbike
+app.get("/getbike",async(req,res)=>{
     try{
-        const a=await pool.query('Select * from "Users"');
+        const a=await pool.query('Select * from "Bikes" where "BikeId"=$1',[req.body.bikeid]);
         console.log(a);
     }
     catch(err)
@@ -29,11 +27,33 @@ app.get("/view",async(req,res)=>{
         console.error(err.message);
     }
 });
-//create
-
-// update 
 
 //delete
+
+app.put("/removebike",async(req,res)=>{
+    try{
+        const a=await pool.query('DELETE FROM "Bikes" WHERE  "BikeId"=$1',[req.body.bikeid]);
+       // console.log("Done");
+    }
+    catch(err)
+    {
+        console.error(err.message);
+    }
+});
+
+app.get("/Bookbike",async(req,res)=>{
+try{
+    const a=await pool.query('Select * from "Bikes" where "BikeStatus"=1 and "LastStationDocked"=$1 limit 1',[req.body.stationid])
+    console.log(a.rows[0].BikeId);
+    await pool.query(' UPDATE "Bikes" SET "BikeStatus"=0 where "BikeId"=$1',[a.rows[0].BikeId]);
+    await pool.query(
+        'INSERT INTO "Trips" ("TripId","UserId","BikeBooked")  VALUES ($1, $2,$3)', [req.body.TripId, req.body.UserId,a.rows[0].BikeId]); 
+}
+catch(err)
+{
+    console.log(err.message);
+}
+})
 
 app.listen(5000,()=>{
 console.log('server is listening on port 5000');
