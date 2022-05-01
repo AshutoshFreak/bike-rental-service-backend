272888e7df435631ac3e19ec457b93afcbe91fd5
divyanshk443 <55971596+divyanshk443@users.noreply.github.com>
2022-05-01 15:18:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const { AddBike } = require("./db/db");
 
+var cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.static('public'))
